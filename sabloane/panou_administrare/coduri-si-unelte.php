209e1689034293f03ad4df0d59d5e38ba6d8a698
BaxAndrei <eftimie.andrei4@gmail.com>
2018-07-21 22:10:03 +0300

    Actualizare 21/07/2018 22:10

---
+++ sabloane/panou_administrare/coduri-si-unelte.php
@@ -0,0 +1,29 @@
+<div class="row" style="margin-left: auto!important;margin-right: auto!important;">
+<div class="col-md-12 col-sm-12 col-xs-12">
+
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title">Cod pentru incorporare in site</h3>
+  </div>
+  <div class="panel-body">
+
+  <pre style="margin:0;"><code style="margin:0;">&lt;!-- Inceput Cod Iframe Dedicatii --&gt;
+&lt;iframe id=&quot;bx-iframe-dedicatii&quot; src=&quot;<?php echo str_replace('admin/coduri-si-unelte','',adresa_curenta()); ?>&quot; width=&quot;600px&quot; height=&quot;500px&quot; style=&quot;margin:0; border:none; overflow:hidden;&quot; scrolling=&quot;no&quot;&gt;&lt;/iframe&gt;
+&lt;!-- Sfarsit Cod Iframe Dedicatii --&gt;</code></pre>
+
+  </div>
+</div>
+
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title">Cod pentru afisare DJ curent si status dedicatii</h3>
+  </div>
+  <div class="panel-body">
+
+  <pre style="margin:0;"><code style="margin:0;">In curand...</code></pre>
+
+  </div>
+</div>
+
+</div>
+</div>
