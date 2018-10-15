23bd16e1ceb44fae373a78089aa8936389f2a942
BaxAndrei <eftimie.andrei4@gmail.com>
2018-10-15 11:45:09 +0300

    Actualizare 15/10/2018 11:44

--- sabloane/panou_administrare/coduri-si-unelte.php
+++ sabloane/panou_administrare/coduri-si-unelte.php
@@ -3,13 +3,13 @@
 
 <div class="panel panel-primary">
   <div class="panel-heading">
-    <h3 class="panel-title">Cod pentru incorporare in site</h3>
+    <h3 class="panel-title">Cod pentru incorporare dedicatii in site</h3>
   </div>
   <div class="panel-body">
 
-  <pre style="margin:0;"><code style="margin:0;">&lt;!-- Inceput Cod Iframe Dedicatii --&gt;
+  <pre style="margin:0;"><code style="margin:0;">&lt;!-- Inceput Cod pentru incorporare dedicatii in site --&gt;
 &lt;iframe id=&quot;bx-iframe-dedicatii&quot; src=&quot;<?php echo adresa_url_site; ?>/&quot; width=&quot;600px&quot; height=&quot;500px&quot; style=&quot;margin:0; border:none; overflow:hidden;&quot; scrolling=&quot;no&quot;&gt;&lt;/iframe&gt;
-&lt;!-- Sfarsit Cod Iframe Dedicatii --&gt;</code></pre>
+&lt;!-- Sfarsit Cod pentru incorporare dedicatii in site --&gt;</code></pre>
 
   </div>
 </div>
@@ -20,7 +20,9 @@
   </div>
   <div class="panel-body">
 
-  <pre style="margin:0;"><code style="margin:0;">In curand...</code></pre>
+    <pre style="margin:0;"><code style="margin:0;">&lt;!-- Inceput Cod pentru afisare DJ curent si status dedicatii --&gt;
+&lt;iframe id=&quot;bx-widget-1&quot; src=&quot;<?php echo adresa_url_site; ?>/widget/status_dj/<?php echo id_radio; ?>&stream=IP_RADIO:PORT_RADIO&quot; width=&quot;300px&quot; height=&quot;120px&quot; style=&quot;margin:0; border:none; overflow:hidden;&quot; scrolling=&quot;no&quot;&gt;&lt;/iframe&gt;
+&lt;!-- Sfarsit Cod pentru afisare DJ curent si status dedicatii --&gt;</code></pre>
 
   </div>
 </div>
